2f03bad8db7db1733a6ab3ccf03c92a89b1e9a74
tanglv <tangchaofalv@163.com>
2019-10-17 15:01:33 +0800

    *) 页面调整

--- js/outPatientQueue.js
+++ js/outPatientQueue.js
@@ -9,12 +9,20 @@ axios.defaults.transformRequest = [function (data) {
 }]
 
 var test = Mock.mock({
-    'roomList|3':[{
-        'no':function(){return Mock.mock('@integer(1, 100)')},
-        'roomName':function(){ return '验光室 '+ Mock.mock({"number|1-100": 0}).number }, 
-        'waiterList|1-10':[{
-            'no':function(){return Mock.mock('@integer(1, 100)')},
-            'name':function(){ return Mock.mock('@cname()');}
+    "queueList|3":[{
+        "queueId":function(){return Mock.mock("@integer(1, 100)");},
+        "queueName":function(){ return "验光室 "+ Mock.mock({"number|1-100": 0}).number; },
+        "callQue|0-1":[{
+            "orders":function(){return Mock.mock("@integer(1, 100)");},
+            "patientName":function(){ return Mock.mock("@cname()");},
+            "opDate":function(){ return Mock.mock("@datetime");},
+            "regNumber":function(){ return Mock.mock("@cname()");}
+        }],
+        "waitingQue|1-10":[{
+            "orders":function(){return Mock.mock("@integer(1, 100)");},
+            "patientName":function(){ return Mock.mock("@cname()");},
+            "opDate":function(){ return Mock.mock("@datetime()");},
+            "regNumber":function(){ return Mock.mock("@cname()");}
         }]
     }]
 });
@@ -41,7 +49,7 @@ new Vue({
     data: function() {
         return {
             dateTime: moment().format('ll,dddd LTS'),
-            dataList: test.roomList
+            dataList: test.queueList
         }
     }
 });
@@ -49,7 +57,7 @@ Vue.component("panel-info", {
     props: {
         title: {
             type: String,
-            default: '标题'
+            default: "标题"
         },
         show_default_slot: {
             type: Boolean,
@@ -58,18 +66,30 @@ Vue.component("panel-info", {
         data: {
             type: Array,
             default: () => []
+        },
+        call:{
+            type: Array,
+            default: () => []
         }
     },
-    template: `  <div class="panel-info">
+    template: ` <div class="panel-info">
                     <div class="panel-info-header">{{title}}</div>
                     <ul class="panel-info-list text-show-font">
-                    
-                        <li v-for='item in data' class="panel-info-list-item">
-                            <span>{{item.name}}</span>
-                            <span>（{{item.no}}）</span>
+                        <li v-for='item in call' class="panel-info-list-item calling-item">
+                            <span>({{item.orders}}.) {{item.patientName}}就诊</span>
                         </li>
+                        <template v-if="call.length>0">
+                            <li v-for='(item,index) in data' v-if="index<7" class="panel-info-list-item">
+                                <span>({{item.orders}}.) {{item.patientName}}</span>
+                            </li>
+                        </template>
+                        <template v-else>
+                            <li v-for='(item,index) in data' v-if="index<8" class="panel-info-list-item">
+                                <span>({{item.orders}}.) {{item.patientName}}</span> 
+                            </li>
+                        </template>
                         <div class="panel-info-list-bg">
-                            <div class="panel-info-list-bg-item" v-once v-for="i in 20">
+                            <div class="panel-info-list-bg-item" v-once v-for="i in 8">
                                 <span class="panel-info-list-bg-item-span">bg</span>
                             </div>
                         </div>
